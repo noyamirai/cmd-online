45d15e7fa979791d55477f32f292ff65d02eaf4f
noyamirai <planet1109@hotmail.com>
2022-03-21 15:01:11 +0100

    feat(overview): added courses overview based on username

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const express = require(`express`);
 const app = express();
 const port = process.env.PORT || 3000;
 
+const rootPath = require(`path`);
+global.appRoot = rootPath.resolve(__dirname);
+
 const db = require(`./config/db`);
-// const schemas = require(`./models/schemas`);
+
+const schemas = require('./models/schemas');
+const CRUD = require(`./controller/crud-operations`);
+
+const acronymGen = require(`./public/js/acronym-generator`);
 
 app.set(`view engine`, `ejs`);
 
@@ -12,8 +19,35 @@ app.use(`/public`, express.static(`public`));
 
 // ROUTES
 app.use('/', require('./routes/home'));
+// app.use('/:username', require('./routes/user'));
 
 db.connectDb();
+
+app.get(`/:username/courses`, (req, res) => {
+    const baseURL = req.path;
+
+    CRUD.findDocByQuery(schemas.User, `username`, req.params.username).then((userData) => {
+        schemas.TeacherCourse.find({
+            'userId': userData.id
+        }).lean().populate(`course`).exec(function(err, courseData) {
+            if (err) Promise.reject(err);
+
+            courseData.forEach(doc => {
+                doc.acronym = acronymGen.createAcronym(doc.course.title);
+            });
+
+            res.render(`courses-overview`, {
+                layout: `default`,
+                profile_pic: userData.profile_pic,
+                userName: `${userData.name.first} ${userData.name.last}`,
+                root: req.baseUrl,
+                baseURL: baseURL,
+                userData: userData,
+                courseData: courseData
+            });
+        });
+    });
+});
 
 app.get(`*`, (req, res) => {
     res.status(404).send(`Page not found!`);
