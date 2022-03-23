4fa2e0c43da9e25f42581f379d748a18ebdc12bc
TejoBurgboy <tejovdburg@gmail.com>
2022-03-23 15:34:46 +0100

    git-pull

--- config/db.js
+++ config/db.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-undef */
-require("dotenv").config();
+require('dotenv').config();
 
-const mongoose = require("mongoose");
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@teamcreator-db.9p0bn.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const mongoose = require('mongoose');
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.8oeua.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const connectDb = async () => {
     try {
         await mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
-        console.log("DB connected");     
+        console.log('DB connected');     
     } catch (error) {
         console.log(`error occured while trying to connect to db: ${error}`);
         throw error;
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-/* eslint-disable quotes */
 /* eslint-disable no-undef */
+require('dotenv').config()
 const express = require(`express`);
 const app = express();
+
+const session = require('express-session');
+const passport = require('passport');
+
 const port = process.env.PORT || 3000;
+const EXPsession = process.env.SecretSESSION;
+
+const db = require(`./config/db`);
 
 app.set(`view engine`, `ejs`);
-
+app.set('views', 'views');
 app.use(`/public`, express.static(`public`));
+app.use('/public', express.static(__dirname + '/public/'));
+
+db.connectDb();
+
+require('./config/verifyUser')(passport);
+
+//EXPRESS MIDDLEWARE
+app.use(express.urlencoded({ extended: false }));
+app.use(session({
+    secret: EXPsession,
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// PASSPORT MIDDLEWARE
+app.use(passport.initialize());
+app.use(passport.session());
 
 // ROUTES
 app.use('/', require('./routes/home'));
+app.use('/users', require('./routes/users'));
+app.use('/:username', require('./routes/overviews'));
 
 app.get(`*`, (req, res) => {
     res.status(404).send(`Page not found!`);
